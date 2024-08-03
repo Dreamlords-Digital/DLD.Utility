e5b2e129ab3769bd1dd59459c8d878c3218f4b5c
Ferdinand Joseph Fernandez <ferdinandjosephfernandez@gmail.com>
2024-08-03 22:23:52 +0800

    Changed RandomUtil to just use UnityEngine.Random

--- Assets/DLD.Utility/Runtime/RandomUtil.cs
+++ Assets/DLD.Utility/Runtime/RandomUtil.cs
@@ -1,15 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Text;
-using UnityEngine;
 using UnityEngine.Assertions;
 
 namespace DLD.Utility
 {
 	public static class RandomUtil
 	{
+		// ------------------------------------------------------------------------------------------------
+
+		/// <summary>
+		/// Return random number between sta and end. Both inclusive.
+		/// </summary>
+		/// <param name="sta"></param>
+		/// <param name="end"></param>
+		/// <returns></returns>
+		public static int Range(int sta, int end)
+		{
+			return UnityEngine.Random.Range(sta, end + 1);
+		}
+
+		public static void ChangeSeed(int newSeed)
+		{
+			UnityEngine.Random.InitState(newSeed);
+		}
+
+		// ------------------------------------------------------------------------------------------------
+
 		static readonly char[] Consonants =
 		{
 			'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'
@@ -27,30 +44,22 @@ namespace DLD.Utility
 
 		public static string GetUniqueKey(int maxSize, int numbersCount = 4)
 		{
-			byte[] data = new byte[1];
-
-			{
-				RngCrypto.GetNonZeroBytes(data);
-				data = new byte[maxSize];
-				RngCrypto.GetNonZeroBytes(data);
-			}
-
-			StringBuilder result = new StringBuilder(maxSize);
+			var result = new StringBuilder(maxSize);
 			bool consonantNow = true;
 			int count = 0;
-			foreach (byte b in data)
+			for (int i = 0; i < maxSize; i++)
 			{
 				if (count >= maxSize - numbersCount)
 				{
-					result.Append(Numbers[b % (Numbers.Length)]);
+					result.Append(Numbers.PickRandomElement());
 				}
 				else if (consonantNow)
 				{
-					result.Append(Consonants[b % (Consonants.Length)]);
+					result.Append(Consonants.PickRandomElement());
 				}
 				else
 				{
-					result.Append(Vowels[b % (Vowels.Length)]);
+					result.Append(Vowels.PickRandomElement());
 				}
 
 				consonantNow = !consonantNow;
@@ -62,18 +71,10 @@ namespace DLD.Utility
 
 		public static string GetNumberString(int length)
 		{
-			byte[] data = new byte[1];
-
+			var result = new StringBuilder(length);
+			for (int i = 0; i < length; i++)
 			{
-				RngCrypto.GetNonZeroBytes(data);
-				data = new byte[length];
-				RngCrypto.GetNonZeroBytes(data);
-			}
-
-			StringBuilder result = new StringBuilder(length);
-			foreach (byte b in data)
-			{
-				result.Append(Numbers[b % (Numbers.Length)]);
+				result.Append(Numbers.PickRandomElement());
 			}
 
 			return result.ToString();
@@ -101,12 +102,12 @@ namespace DLD.Utility
 		{
 			if (array == null)
 			{
-				return default(T);
+				return default;
 			}
 
 			if (array.Length == 0)
 			{
-				return default(T);
+				return default;
 			}
 
 			return array[Range(0, array.Length - 1)];
@@ -122,14 +123,14 @@ namespace DLD.Utility
 		{
 			if (collection == null)
 			{
-				return default(T);
+				return default;
 			}
 
 			var count = collection.Count;
 
 			if (count == 0)
 			{
-				return default(T);
+				return default;
 			}
 
 			Assert.IsTrue(count >= 1);
@@ -141,7 +142,7 @@ namespace DLD.Utility
 		{
 			if (collection == null)
 			{
-				return default(T);
+				return default;
 			}
 
 			var collectionList = collection as IList<T> ?? collection.ToList();
@@ -149,7 +150,7 @@ namespace DLD.Utility
 
 			if (count == 0)
 			{
-				return default(T);
+				return default;
 			}
 
 			Assert.IsTrue(count >= 1);
@@ -188,43 +189,6 @@ namespace DLD.Utility
 
 		// ------------------------------------------------------------------------------------------------
 
-		public static bool IsRangeAllowed(int sta, int end)
-		{
-			int intRange = end - sta + 1;
-			return intRange is > 0 and < 255;
-		}
-
-		public static int Range(int sta, int end)
-		{
-			// Examples:
-			//
-			// 7 to 12
-			// (12 - 7 + 1) = 6
-			// (1 to 6) + 7 - 1 = 7 to 12
-			//
-			// 1 to 9
-			// (9 - 1 + 1) = 9
-			// (1 to 9) + 1 - 1 = 1 to 9
-			//
-			// 0 to 9
-			// (9 - 0 + 1) = 10
-			// (1 to 10) + 0 - 1 = 0 to 9
-
-			int intRange = end - sta + 1;
-			if (intRange >= 255)
-			{
-				Debug.LogAssertion(
-					$"RNG.Range: Range between {sta.ToString()} to {end.ToString()} ({intRange.ToString()}) should be not more than 255");
-				return 0;
-			}
-
-			byte range = (byte)intRange;
-
-			return RollDiceRngCrypto(range) + sta - 1;
-		}
-
-		// ------------------------------------------------------------------------------------------------
-
 		public static int RollDice(int numberOfDice, int numberOfSides)
 		{
 			if (numberOfDice == 0 || numberOfSides == 0)
@@ -252,7 +216,7 @@ namespace DLD.Utility
 
 			for (int n = 0; n < numberOfDice; ++n)
 			{
-				result += RollDice(numberOfSides);
+				result += Range(1, numberOfSides);
 			}
 
 			//Debug.Log($"RandomUtil.RollDice: dice roll {numberOfDice}d{numberOfSides}+{bonus} returned {result}");
@@ -260,52 +224,6 @@ namespace DLD.Utility
 			return result;
 		}
 
-		public static int RollDice(int numberOfSides)
-		{
-			Assert.IsTrue(numberOfSides is >= 2 and <= 255,
-				$"number of sides has to be within 2 to 255. was: {numberOfSides}");
-
-			return RollDiceRngCrypto((byte)numberOfSides);
-		}
-
 		// ------------------------------------------------------------------------------------------------
-		// current implementation is using RNGCryptoServiceProvider
-
-		static readonly RNGCryptoServiceProvider RngCrypto = new RNGCryptoServiceProvider();
-
-		static readonly byte[] RandomNumber = new byte[1];
-
-		static byte RollDiceRngCrypto(byte numberSides)
-		{
-			if (numberSides <= 0)
-			{
-				return 0;
-			}
-
-			do
-			{
-				// Fill the array with a random value.
-				RngCrypto.GetBytes(RandomNumber);
-			} while (!IsFairRoll(RandomNumber[0], numberSides));
-
-			// Return the random number mod the number of sides.
-			// The possible values are zero-based, so we add one.
-			return (byte)((RandomNumber[0] % numberSides) + 1);
-		}
-
-		static bool IsFairRoll(byte roll, byte numSides)
-		{
-			// There are MaxValue / numSides full sets of numbers that can come up
-			// in a single byte.  For instance, if we have a 6 sided die, there are
-			// 42 full sets of 1-6 that come up.  The 43rd set is incomplete.
-			int fullSetsOfValues = Byte.MaxValue / numSides;
-
-			// If the roll is within this range of fair values, then we let it continue.
-			// In the 6 sided die case, a roll between 0 and 251 is allowed.  (We use
-			// < rather than <= since the = portion allows through an extra 0 value).
-			// 252 through 255 would provide an extra 0, 1, 2, 3 so they are not fair
-			// to use.
-			return roll < numSides * fullSetsOfValues;
-		}
 	}
 }
