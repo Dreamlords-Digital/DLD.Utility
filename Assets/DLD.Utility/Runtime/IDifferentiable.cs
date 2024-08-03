90565a103c2b9870062b393fc4b8a1d8d2aec1f8
Ferdinand Joseph Fernandez <ferdinandjosephfernandez@gmail.com>
2024-08-04 04:29:33 +0800

    Added IDifferentiable

---
+++ Assets/DLD.Utility/Runtime/IDifferentiable.cs
@@ -0,0 +1,21 @@
+﻿// COPYRIGHT (C) DREAMLORDS DIGITAL INC. - ALL RIGHTS RESERVED.
+
+namespace DLD.Utility
+{
+	/// <summary>
+	/// Adds a difference checker with another instance.
+	/// Instead of overriding the Equals method, this makes
+	/// it apparent that the class has a value-based equality checker.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public interface IDifferentiable<in T>
+	{
+		/// <summary>
+		/// Method for checking whether another instance's data is different from self.
+		/// This is a value-based equality checker.
+		/// </summary>
+		/// <param name="other"></param>
+		/// <returns></returns>
+		bool IsDifferentFromMe(T other);
+	}
+}
