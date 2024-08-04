55d48f466586c32e2116cf0f7978878d48dce044
Ferdinand Joseph Fernandez <ferdinandjosephfernandez@gmail.com>
2024-08-05 00:02:42 +0800

    Added StringUtil.ComputeMD5Hash

--- Assets/DLD.Utility/Runtime/StringUtil.cs
+++ Assets/DLD.Utility/Runtime/StringUtil.cs
@@ -188,5 +188,21 @@ namespace DLD.Utility
 
 			return true;
 		}
+
+		public static string ComputeMD5Hash(string input)
+		{
+			// Use input string to calculate MD5 hash
+			using var md5 = System.Security.Cryptography.MD5.Create();
+			byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+			byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+			// Convert the byte array to hexadecimal string
+			var sb = new StringBuilder();
+			for (int i = 0; i < hashBytes.Length; i++)
+			{
+				sb.Append(hashBytes[i].ToString("X2"));
+			}
+			return sb.ToString();
+		}
 	}
 }
