f773e46b44aa6d7584cb8a981089e6729865ff08
Ferdinand Joseph Fernandez <ferdinandjosephfernandez@gmail.com>
2024-08-04 23:10:19 +0800

    Added some Rect utility code in DLD.IMGUI

--- Assets/DLD.IMGUI/Runtime/Utility.cs
+++ Assets/DLD.IMGUI/Runtime/Utility.cs
@@ -135,6 +135,154 @@ namespace DLD.IMGUI
 		{
 			return hasFocus ? "SearchClearButton.Focused" : "SearchClearButton";
 		}
+		public static void GetAvailableSize(out int width, out int height)
+		{
+#if UNITY_EDITOR
+			if (UnityEditor.EditorApplication.isPlaying)
+			{
+#endif
+				width = Screen.width;
+				height = Screen.height;
+				return;
+#if UNITY_EDITOR
+			}
+#endif
+
+#if UNITY_EDITOR
+			const int SCENE_VIEW_UPPER_BAR_HEIGHT = 17;
+			if (UnityEditor.EditorWindow.focusedWindow != null && UnityEditor.EditorWindow.focusedWindow.GetType() == typeof(UnityEditor.SceneView))
+			{
+				//return Mathf.RoundToInt(EditorWindow.focusedWindow.position.height) + ALLOWANCE - UPPER_BAR_HEIGHT;
+
+				var size = UnityEditor.EditorWindow.focusedWindow.position;
+				width = Mathf.RoundToInt(size.width);
+				height = Mathf.RoundToInt(size.height) - SCENE_VIEW_UPPER_BAR_HEIGHT;
+				return;
+			}
+			else if (UnityEditor.SceneView.lastActiveSceneView != null)
+			{
+				// SceneView.lastActiveSceneView seems to be buggy, or we are misunderstanding what it means.
+				// if you have two scene views as tabs in the same area, SceneView.lastActiveSceneView seems
+				// to always point to the leftmost one, even if it's not the active/focused scene view.
+				var size = UnityEditor.SceneView.lastActiveSceneView.position;
+				width = Mathf.RoundToInt(size.width);
+				height = Mathf.RoundToInt(size.height) - SCENE_VIEW_UPPER_BAR_HEIGHT;
+				return;
+			}
+#endif
+			width = Screen.width;
+			height = Screen.height;
+		}
+
+		/// <summary>
+		/// Fits Rect position into the Screen as much as possible. When in the Unity Editor during Edit Mode, this will use the SceneView window's size
+		/// (minus the height of the SceneView's top toolbar).
+		///
+		/// The Rect's size is expected to be smaller than the screen. If not, set <see cref="allowChangingWidth"/> and <see cref="allowChangingHeight"/> to true to allow
+		/// it to change the Rect's size to fit into the screen.
+		///
+		/// The Rect's initial position values are expected to be global screen coordinates,
+		/// meaning (0, 0) is top-left of entire screen, (width, height) is bottom-right of entire screen.
+		/// </summary>
+		/// <param name="rectToAdjust">The Rect values to adjust.</param>
+		/// <param name="screenMarginLeft">Distance from the left edge of the screen to give the tooltip some space, if it was adjusted to be anchored to the left edge of the screen.</param>
+		/// <param name="screenMarginRight">Distance from the right edge of the screen to give the tooltip some space, if it was adjusted to be anchored to the right edge of the screen.</param>
+		/// <param name="screenMarginTop">Distance from the top edge of the screen to give the tooltip some space, if it was adjusted to be anchored to the top edge of the screen.</param>
+		/// <param name="screenMarginBottom">Distance from the bottom edge of the screen to give the tooltip some space, if it was adjusted to be anchored to the bottom edge of the screen.</param>
+		/// <param name="allowChangingWidth">If the Rect is larger than the screen width, set this to true to allow it to shrink the Rect width to fit in the screen.</param>
+		/// <param name="allowChangingHeight">If the Rect is larger than the screen height, set this to true to allow it to shrink the Rect height to fit in the screen.</param>
+		/// <returns></returns>
+		public static Rect FitInScreen(this Rect rectToAdjust,
+			float screenMarginLeft, float screenMarginRight, float screenMarginTop, float screenMarginBottom,
+			bool allowChangingWidth = false, bool allowChangingHeight = false)
+		{
+			GetAvailableSize(out var screenWidth, out var screenHeight);
+
+			if (rectToAdjust.height > screenHeight && allowChangingHeight)
+			{
+				// rect is too big for screen height
+				// just fill entire screen height
+				rectToAdjust.y = screenMarginTop;
+				rectToAdjust.height = screenHeight - screenMarginTop - screenMarginBottom;
+			}
+			else
+			{
+				bool tooFarUp = rectToAdjust.y < 0;
+				bool tooFarDown = rectToAdjust.yMax > screenHeight;
+
+				if (tooFarUp && !tooFarDown)
+				{
+					// if rect is too far up the screen, bring it down
+					rectToAdjust.y = screenMarginTop;
+				}
+				else if (!tooFarUp && tooFarDown)
+				{
+					// if rect is too far down the screen, bring it up
+					rectToAdjust.y = screenHeight - rectToAdjust.height - screenMarginBottom;
+				}
+			}
+
+			if (rectToAdjust.width > screenWidth && allowChangingWidth)
+			{
+				// rect is too big for screen width
+				// just fill entire screen width
+				rectToAdjust.x = screenMarginLeft;
+				rectToAdjust.width = screenWidth - screenMarginLeft - screenMarginRight;
+			}
+			else
+			{
+				bool tooFarToTheLeft = rectToAdjust.x < 0;
+				bool tooFarToTheRight = rectToAdjust.xMax > screenWidth;
+
+				if (tooFarToTheLeft && !tooFarToTheRight)
+				{
+					// if rect is too far to the left of the screen, move it to the right
+					rectToAdjust.x = screenMarginLeft;
+				}
+				else if (!tooFarToTheLeft && tooFarToTheRight)
+				{
+					// if rect is too far to the right of the screen, move it to the left
+					rectToAdjust.x = screenWidth - rectToAdjust.width - screenMarginRight;
+				}
+			}
+
+			return rectToAdjust;
+		}
+
+		/// <summary>
+		/// Fits Rect position into the Screen as much as possible. When in the Unity Editor during Edit Mode, this will use the SceneView window's size
+		/// (minus the height of the SceneView's top toolbar).
+		///
+		/// The Rect's size is expected to be smaller than the screen. If not, set <see cref="allowChangingWidth"/> and <see cref="allowChangingHeight"/> to true to allow
+		/// it to change the Rect's size to fit into the screen.
+		///
+		/// The Rect's initial position values are expected to be global screen coordinates,
+		/// meaning (0, 0) is top-left of entire screen, (width, height) is bottom-right of entire screen.
+		///
+		/// If the Rect's X position needs adjustment, it will move the X position to the specified <see cref="alternativeXPos"/>.
+		/// </summary>
+		/// <param name="rectToAdjust">The Rect values to adjust.</param>
+		/// <param name="alternativeXPos">If after being adjusted, the Rect ends up covering the mouse, it will move the Rect's X position to this value.</param>
+		/// <param name="screenMarginLeft">Distance from the left edge of the screen to give the tooltip some space, if it was adjusted to be anchored to the left edge of the screen.</param>
+		/// <param name="screenMarginRight">Distance from the right edge of the screen to give the tooltip some space, if it was adjusted to be anchored to the right edge of the screen.</param>
+		/// <param name="screenMarginTop">Distance from the top edge of the screen to give the tooltip some space, if it was adjusted to be anchored to the top edge of the screen.</param>
+		/// <param name="screenMarginBottom">Distance from the bottom edge of the screen to give the tooltip some space, if it was adjusted to be anchored to the bottom edge of the screen.</param>
+		/// <param name="allowChangingWidth">If the Rect is larger than the screen width, set this to true to allow it to shrink the Rect width to fit in the screen.</param>
+		/// <param name="allowChangingHeight">If the Rect is larger than the screen height, set this to true to allow it to shrink the Rect height to fit in the screen.</param>
+		/// <returns></returns>
+		public static Rect FitInScreenWithXFlip(this Rect rectToAdjust, float alternativeXPos,
+			float screenMarginLeft, float screenMarginRight, float screenMarginTop, float screenMarginBottom, bool allowChangingWidth = false, bool allowChangingHeight = false)
+		{
+			float initialXPos = rectToAdjust.x;
+			rectToAdjust = rectToAdjust.FitInScreen(screenMarginLeft, screenMarginRight, screenMarginTop, screenMarginBottom);
+
+			if (!Mathf.Approximately(initialXPos, rectToAdjust.x))
+			{
+				rectToAdjust.x = alternativeXPos;
+			}
+
+			return rectToAdjust;
+		}
 	}
 
 }
