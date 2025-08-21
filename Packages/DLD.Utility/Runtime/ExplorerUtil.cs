e13c22b31698b0b9a090914aa7b87b761629b25e
Ferdinand Joseph Fernandez <ferdinandjosephfernandez@gmail.com>
2025-08-22 01:46:26 +0800

    Implemented OpenInFileBrowser for Linux

--- Packages/DLD.Utility/Runtime/ExplorerUtil.cs
+++ Packages/DLD.Utility/Runtime/ExplorerUtil.cs
@@ -1,6 +1,8 @@
 ﻿// COPYRIGHT (C) DREAMLORDS DIGITAL INC. - ALL RIGHTS RESERVED.
 
+using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public static class ExplorerUtil
@@ -37,7 +39,7 @@ public static class ExplorerUtil
 		//Debug.Log("arguments: " + arguments);
 		try
 		{
-			System.Diagnostics.Process.Start("open", arguments);
+			Process.Start("open", arguments);
 		}
 		catch(System.ComponentModel.Win32Exception e)
 		{
@@ -66,7 +68,7 @@ public static class ExplorerUtil
 		}
 		try
 		{
-			System.Diagnostics.Process.Start("explorer.exe", (openInsidesOfFolder ? "/root," : "/select,") + winPath);
+			Process.Start("explorer.exe", (openInsidesOfFolder ? "/root," : "/select,") + winPath);
 		}
 		catch(System.ComponentModel.Win32Exception e)
 		{
@@ -77,9 +79,78 @@ public static class ExplorerUtil
 		}
 	}
 
+	public static void OpenInLinuxFileBrowser(string path)
+	{
+		if (string.IsNullOrWhiteSpace(path))
+		{
+			return;
+		}
+
+		string linuxPath = path.Replace("\\", "/"); // linux doesn't like backward slashes
+
+		if (File.Exists(linuxPath))
+		{
+			// from https://stackoverflow.com/a/73409251
+			using Process dbusShowItemsProcess = new Process
+			{
+				StartInfo = new ProcessStartInfo
+				{
+					FileName = "dbus-send",
+					Arguments = "--print-reply --dest=org.freedesktop.FileManager1 /org/freedesktop/FileManager1 org.freedesktop.FileManager1.ShowItems array:string:\"file://" +
+					            linuxPath +
+					            "\" string:\"\"",
+					UseShellExecute = true
+				}
+			};
+			dbusShowItemsProcess.Start();
+			dbusShowItemsProcess.WaitForExit();
+
+			if (dbusShowItemsProcess.ExitCode == 0)
+			{
+				// dbus ran successfully
+				return;
+			}
+		}
+
+		// The dbus invocation can fail for a variety of reasons:
+		// - dbus is not available
+		// - no programs implement the service,
+		// - ...
+
+		// Attempt xdg-open instead
+
+		if (File.Exists(linuxPath))
+		{
+			// xdg-open will open the file, so get the directory path instead
+			linuxPath = Path.GetDirectoryName(linuxPath);
+		}
+
+		//Debug.Log("macPath: " + macPath);
+		//Debug.Log("openInsidesOfFolder: " + openInsidesOfFolder);
+
+		if (!linuxPath.StartsWith("\""))
+		{
+			linuxPath = "\"" + linuxPath;
+		}
+		if (!linuxPath.EndsWith("\""))
+		{
+			linuxPath = linuxPath + "\"";
+		}
+		string arguments = linuxPath;
+		//Debug.Log("arguments: " + arguments);
+		try
+		{
+			Process.Start("xdg-open", arguments);
+		}
+		catch(System.ComponentModel.Win32Exception e)
+		{
+			e.HelpLink = ""; // do anything with this variable to silence warning about not using it
+		}
+	}
+
 	public static void OpenWithDefaultProgram(string path)
 	{
-		System.Diagnostics.Process.Start(path);
+		Process.Start(path);
 	}
 
 	public static void OpenInFileBrowser(string path)
@@ -89,8 +160,18 @@ public static class ExplorerUtil
 			return;
 		}
 
-		OpenInWinFileBrowser(path);
-		OpenInMacFileBrowser(path);
+		if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+		{
+			OpenInWinFileBrowser(path);
+		}
+		else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+		{
+			OpenInMacFileBrowser(path);
+		}
+		else // assume Linux
+		{
+			OpenInLinuxFileBrowser(path);
+		}
 	}
 
 }
