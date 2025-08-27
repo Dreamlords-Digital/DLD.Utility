26e59580800ea47d38c9fcb4976f45d875862f5f
Ferdinand Joseph Fernandez <ferdinandjosephfernandez@gmail.com>
2025-08-27 20:53:36 +0800

    Added styles for warning/error indications

--- Packages/com.dreamlordsdigital.uitoolkit/Runtime/BaseStyles.cs
+++ Packages/com.dreamlordsdigital.uitoolkit/Runtime/BaseStyles.cs
@@ -4,6 +4,8 @@ namespace DLD.UIToolkit
 {
     public static class BaseStyles
     {
+        public const string BOX_WITH_WARNING_STYLE_CLASS = "dld-box--warning";
+        public const string BOX_WITH_ERROR_STYLE_CLASS = "dld-box--error";
         public const string TEXT_FIELD_WITH_ERROR_STYLE_CLASS = "dld-text-field--error";
         public const string LABEL_WITH_WARNING_STYLE_CLASS = "dld-label--warning";
         public const string LABEL_WITH_ERROR_STYLE_CLASS = "dld-label--error";
