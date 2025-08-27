d69b5e441d8577c5c840bde5c0ab0549ed3796a8
Ferdinand Joseph Fernandez <ferdinandjosephfernandez@gmail.com>
2025-08-27 21:52:42 +0800

    Update to Base Style

--- Packages/com.dreamlordsdigital.uitoolkit/Runtime/BaseStyles.cs
+++ Packages/com.dreamlordsdigital.uitoolkit/Runtime/BaseStyles.cs
@@ -11,5 +11,7 @@ namespace DLD.UIToolkit
         public const string LABEL_WITH_ERROR_STYLE_CLASS = "dld-label--error";
 
         public const string TOGGLE_WITH_ICON_STYLE_CLASS = "dld-grid-toggle--with-icon";
+
+        public const string SCROLL_VIEW_WITH_EXPANDED_WIDTH_STYLE_CLASS = "dld-scroll-view--expand-width";
     }
 }
