708a6cce7cf798d74c48f88e75155d795ff466bd
Ferdinand Joseph Fernandez <ferdinandjosephfernandez@gmail.com>
2025-09-04 04:06:29 +0800

    Added SearchTextField for UIToolkit Utility

---
+++ Packages/com.dreamlordsdigital.uitoolkit/Runtime/SearchTextField.cs
@@ -0,0 +1,216 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace DLD.UIToolkit
+{
+	public enum SearchType : byte
+	{
+		SimpleNotMatchCase,
+		SimpleMatchCase,
+		Fuzzy,
+		Regex,
+	}
+
+	public interface ISearchTextFieldListener
+	{
+		void OnSearchTextChanged(SearchType searchType, string searchText);
+	}
+
+	[UxmlElement]
+	public partial class SearchTextField : TextField, IContextMenuListener
+	{
+		const string SEARCH_TYPE_SIMPLE = "SEARCH_TYPE_SIMPLE";
+		const string SEARCH_TYPE_FUZZY = "SEARCH_TYPE_FUZZY";
+		const string SEARCH_TYPE_REGEX = "SEARCH_TYPE_REGEX";
+		const string MATCH_CASE = "MATCH_CASE";
+
+		const float DEFAULT_SEARCH_DELAY = 0.33f;
+
+		// =====================================================================
+
+		SearchType _searchType = SearchType.Fuzzy;
+		bool _matchCase;
+		bool _wasInFocusBeforeClickingOnSearchButton;
+		float _searchDelayRemaining;
+
+		readonly Toggle _searchButton;
+
+		IContextMenu _contextMenu;
+		ISearchTextFieldListener _listener;
+
+		// =====================================================================
+
+		public SearchTextField()
+		{
+			keyboardType = TouchScreenKeyboardType.Search;
+			AddToClassList("dld-search-text-field");
+			RegisterCallback<FocusOutEvent, SearchTextField>((e, me) => me.OnFocusOut(e), this);
+
+			_searchButton = new Toggle();
+			_searchButton.RegisterCallback<ClickEvent, SearchTextField>((e, me) => me.OnSearchButtonPressed(), this);
+			_searchButton.AddToClassList("dld-search-text-field__search-button");
+
+			var textBox = this.Q<VisualElement>("unity-text-input");
+			textBox.Add(_searchButton);
+
+			if (isDelayed)
+			{
+				RegisterCallback<ChangeEvent<string>, SearchTextField>((e, me) => me.OnDelayedChange(e), this);
+			}
+			else
+			{
+				RegisterCallback<ChangeEvent<string>, SearchTextField>((e, me) => me.OnImmediateChange(e), this);
+			}
+		}
+
+		public void SetContextMenu(IContextMenu newContextMenu)
+		{
+			_contextMenu = newContextMenu;
+		}
+
+		public void SetListener(ISearchTextFieldListener newListener)
+		{
+			_listener = newListener;
+		}
+
+		// =====================================================================
+
+		public void Update(float deltaTimeSeconds)
+		{
+			if (_searchDelayRemaining > 0)
+			{
+				_searchDelayRemaining -= deltaTimeSeconds;
+				if (_searchDelayRemaining <= 0)
+				{
+					_listener?.OnSearchTextChanged(_searchType, value);
+				}
+			}
+		}
+
+		void OnDelayedChange(ChangeEvent<string> e)
+		{
+			_listener?.OnSearchTextChanged(_searchType, e.newValue);
+		}
+
+		void OnImmediateChange(ChangeEvent<string> e)
+		{
+			_searchDelayRemaining = DEFAULT_SEARCH_DELAY;
+		}
+
+		void OnFocusOut(FocusOutEvent e)
+		{
+			if (e.relatedTarget != null && e.relatedTarget is VisualElement elementThatGainedFocus)
+			{
+				if (IndexOf(elementThatGainedFocus) == -1)
+				{
+					// not ours
+					_wasInFocusBeforeClickingOnSearchButton = false;
+				}
+			}
+			else
+			{
+				_wasInFocusBeforeClickingOnSearchButton = true;
+			}
+		}
+
+		void OnSearchButtonPressed()
+		{
+			Debug.Assert(_contextMenu != null, "_contextMenu should be assigned");
+
+			bool searchTypeIsSimple = _searchType is SearchType.SimpleNotMatchCase or SearchType.SimpleMatchCase;
+
+			ContextMenuItemStyle matchCaseStyle;
+			string matchCaseTooltip;
+			if (searchTypeIsSimple)
+			{
+				matchCaseTooltip = "Case sensitive or not.";
+				if (_matchCase)
+				{
+					matchCaseStyle = ContextMenuItemStyle.Selected;
+				}
+				else
+				{
+					matchCaseStyle = ContextMenuItemStyle.Standard;
+				}
+			}
+			else
+			{
+				matchCaseTooltip = "Only applies to Simple Search Type.";
+				if (_matchCase)
+				{
+					matchCaseStyle = ContextMenuItemStyle.Selected | ContextMenuItemStyle.Disabled;
+				}
+				else
+				{
+					matchCaseStyle = ContextMenuItemStyle.Standard | ContextMenuItemStyle.Disabled;
+				}
+			}
+
+			_contextMenu.ClearMenu();
+			_contextMenu.AddLabel("Search Type:");
+			_contextMenu.AddMenu("Simple", tooltip: "Use * as wildcard.",
+				menuItemStyle: searchTypeIsSimple ? ContextMenuItemStyle.Selected : ContextMenuItemStyle.Standard,
+				listener: this, userArg1: SEARCH_TYPE_SIMPLE);
+			_contextMenu.AddMenu("Fuzzy", tooltip: "Use fuzzy search algorithm. Fuzzy search is lenient on wrong spellings.",
+				menuItemStyle: _searchType == SearchType.Fuzzy ? ContextMenuItemStyle.Selected : ContextMenuItemStyle.Standard,
+				listener: this, userArg1: SEARCH_TYPE_FUZZY);
+			_contextMenu.AddMenu("Regex", tooltip: "Use regular expression.",
+				menuItemStyle: _searchType == SearchType.Regex ? ContextMenuItemStyle.Selected : ContextMenuItemStyle.Standard,
+				listener: this, userArg1: SEARCH_TYPE_REGEX);
+			_contextMenu.AddSeparator();
+			_contextMenu.AddMenu("Match Case", tooltip: matchCaseTooltip,
+				menuItemStyle: matchCaseStyle,
+				listener: this, userArg1: MATCH_CASE);
+			_contextMenu.Show(this, ContextMenuAnchorPoint.LowerRight, listener: this);
+		}
+
+		public void OnContextMenuChosen(int index, Label itemLabel, object itemTooltip, object userArg1, object userArg2)
+		{
+			switch (userArg1 as string)
+			{
+				case SEARCH_TYPE_SIMPLE:
+					if (_searchType is SearchType.SimpleNotMatchCase or SearchType.SimpleMatchCase)
+					{
+						return;
+					}
+					_searchType = _matchCase ? SearchType.SimpleMatchCase : SearchType.SimpleNotMatchCase;
+					break;
+				case SEARCH_TYPE_FUZZY:
+					if (_searchType == SearchType.Fuzzy)
+					{
+						return;
+					}
+					_searchType = SearchType.Fuzzy;
+					break;
+				case SEARCH_TYPE_REGEX:
+					if (_searchType == SearchType.Regex)
+					{
+						return;
+					}
+					_searchType = SearchType.Regex;
+					break;
+				case MATCH_CASE:
+					_matchCase = !_matchCase;
+					if (_searchType is SearchType.SimpleNotMatchCase or SearchType.SimpleMatchCase)
+					{
+						_searchType = _matchCase ? SearchType.SimpleMatchCase : SearchType.SimpleNotMatchCase;
+					}
+					break;
+			}
+			_listener?.OnSearchTextChanged(_searchType, value);
+		}
+
+		public void OnContextMenuClosed(bool userCancelled)
+		{
+			_searchButton.SetValueWithoutNotify(false);
+
+			if (_wasInFocusBeforeClickingOnSearchButton)
+			{
+				Focus();
+				_wasInFocusBeforeClickingOnSearchButton = false;
+			}
+		}
+
+		// =====================================================================
+	}
+}
