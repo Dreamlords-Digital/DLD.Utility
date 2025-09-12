047c8537c8ed3fa245d337b4e508031623978c0f
Ferdinand Joseph Fernandez <ferdinandjosephfernandez@gmail.com>
2025-09-12 11:59:10 +0800

    Removed tooltip from EnumUI since it's no longer needed

--- Packages/com.dreamlordsdigital.uitoolkit/Runtime/EnumUIAttribute.cs
+++ Packages/com.dreamlordsdigital.uitoolkit/Runtime/EnumUIAttribute.cs
@@ -1,19 +1,15 @@
 using System;
-using UnityEngine;
-using UnityEngine.UIElements;
 
 namespace DLD.UIToolkit
 {
 	public class EnumUIAttribute : Attribute
 	{
-		public readonly string Tooltip;
 		public readonly string Label;
 		public readonly string ShortLabel;
 		public readonly string IconStyleClass;
 
-		public EnumUIAttribute(string tooltip = null, string label = null, string shortLabel = null, string iconStyleClass = null)
+		public EnumUIAttribute(string label = null, string shortLabel = null, string iconStyleClass = null)
 		{
-			Tooltip = tooltip;
 			Label = label;
 			ShortLabel = shortLabel;
 			IconStyleClass = iconStyleClass;
