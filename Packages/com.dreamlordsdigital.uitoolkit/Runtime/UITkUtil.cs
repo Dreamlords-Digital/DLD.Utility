00b96dd4a6fa19acb8d2b1a7faade0a221bd6c6a
Ferdinand Joseph Fernandez <ferdinandjosephfernandez@gmail.com>
2026-02-01 20:32:45 +0800

    Minor edit, added comments on command names

--- Packages/com.dreamlordsdigital.uitoolkit/Runtime/UITkUtil.cs
+++ Packages/com.dreamlordsdigital.uitoolkit/Runtime/UITkUtil.cs
@@ -9,15 +9,51 @@ namespace DLD.UIToolkit
 
 public static class CommandName
 {
+	/// <summary>
+	/// Default shortcut: Ctrl + F
+	/// </summary>
 	public const string Find = "Find";
+
+	/// <summary>
+	/// Default shortcut: Ctrl + X
+	/// </summary>
 	public const string Cut = "Cut";
+
+	/// <summary>
+	/// Default shortcut: Ctrl + C
+	/// </summary>
 	public const string Copy = "Copy";
+
+	/// <summary>
+	/// Default shortcut: Ctrl + V
+	/// </summary>
 	public const string Paste = "Paste";
+
+	/// <summary>
+	/// Default shortcut: Ctrl + D
+	/// </summary>
 	public const string Duplicate = "Duplicate";
+
+	/// <summary>
+	/// Default shortcut: Ctrl + A
+	/// </summary>
 	public const string SelectAll = "SelectAll";
+
+	/// <summary>
+	/// Default shortcut: Shift + D
+	/// </summary>
 	public const string DeselectAll = "DeselectAll";
+
+	/// <summary>
+	/// Default shortcut: Shift + Delete
+	/// </summary>
 	public const string Delete = "Delete";
+
+	/// <summary>
+	/// Default shortcut: Delete
+	/// </summary>
 	public const string SoftDelete = "SoftDelete";
+
 	public const string FrameSelected = "FrameSelected";
 	public const string FrameSelectedWithLock = "FrameSelectedWithLock";
 	public const string FocusProjectWindow = "FocusProjectWindow";
